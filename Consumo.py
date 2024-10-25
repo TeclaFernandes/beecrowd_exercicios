c199e6490c8581bcfcb402bf9a0821d5c9b0bb39
Tecla Fernandes Oliveira <115437392+TeclaFernandes@users.noreply.github.com>
2024-10-25 16:39:48 -0300

    1014

---
+++ Consumo.py
@@ -0,0 +1,9 @@
+#REEBENDO A DISTANNCIA E O CNSUMO DE COMBSTIVEL:
+X = int(input())
+Y = float(input())
+
+#CALCULANDO O CONSUMO MÉDIO:
+consumo = X/Y
+
+#IMPRIMINDO O VALOR:
+print(f'{consumo:.3f} km/l')
